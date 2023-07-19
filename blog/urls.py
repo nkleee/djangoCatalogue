6e0439e64d37018c352718ad074ffc49027362dc
nkleee <nogi3001@gmail.com>
2023-07-19 09:51:59 +0900

    230719 슬러그 url 패턴 정규표현식 변경

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from blog import views
 
@@ -6,5 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'<int:year>/<int:month>/<int:day>/<post>/', views.post_detail, name='post_detail'),
+    re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$', views.post_detail,
+            name='post_detail'),
 ]
