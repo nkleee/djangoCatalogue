687408d90954841fe56b1b006d75e2dcdd2d4c7e
nkleee <nogi3001@gmail.com>
2023-07-31 10:48:26 +0900

    사용자 등록 뷰 함수 추가, 비밀번호 해싱 알고리즘 변경

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -30,3 +30,23 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # 새로운 사용자 객체를 생성하지만 아직 저장하지는 않습니다.
+            new_user = user_form.save(commit=False)
+            # 선택한 비밀번호를 설정합니다.
+            new_user.set_password(
+                user_form.cleaned_date['password'])
+            # 사용자 객체를 저장합니다.
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user':new_user})
+        else:
+            user_form = UserRegistrationForm()
+        return render(request,
+                      'account/register.html',
+                      {'user_form':user_form})
--- mysite/settings.py
+++ mysite/settings.py
@@ -125,14 +125,22 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = ''
-# EMAIL_HOST_PASSWORD = ''
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-#
-# LOGIN_URL = 'login'
-# LOGOUT_URL = 'logout'
-# LOGIN_REDIRECT_URL = 'dashboard'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'dashboard'
+
+PASSWORD_HASHERS = [
+ 'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+ 'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+ 'django.contrib.auth.hashers.Argon2PasswordHasher',
+ 'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+ 'django.contrib.auth.hashers.ScryptPasswordHasher',
+ ]
