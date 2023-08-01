f85f205f57ce6dd20962f04d20c627f9ba1ac813
nkleee <nogi3001@gmail.com>
2023-08-01 09:18:19 +0900

    메세지 프레임워크 추가 및 수정 함수 메시지 성공, 실패 추가

--- account/views.py
+++ account/views.py
@@ -1,52 +1,62 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
+
+
+class CustomLoginView(LoginView):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        return super().dispatch(request, *args, **kwargs)
 
 
 # Create your views here.
-
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
-    return render(request, 'account/register.html', {'form': form})
-
-
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
 
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # 새로운 사용자 객체를 생성하지만 아직 저장하지는 않습니다.
             new_user = user_form.save(commit=False)
-            # 선택한 비밀번호를 설정합니다.
-            new_user.set_password(
-                user_form.cleaned_date['password'])
-            # 사용자 객체를 저장합니다.
+            new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            from account.models import Profile
+            Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
-                          {'new_user':new_user})
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully')
         else:
-            user_form = UserRegistrationForm()
-        return render(request,
-                      'account/register.html',
-                      {'user_form':user_form})
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
